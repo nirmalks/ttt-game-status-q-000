9dffbcd2263c934e0b1f12f963d7b1953e54c03d
Nirmal Kumar K S <kagosekar@gmail.com>
2015-10-22 13:45:52 +0000

    nitrous-backup at Thu Oct 22 13:45:52 UTC 2015

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,19 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won(board)
+  WIN_COMBINATIONS.each do |combi|
+    board_val.each do |combi_element|
+      pos = board[combi_element]
+end
